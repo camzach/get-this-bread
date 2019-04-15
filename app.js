20d8b1dd0e96d1039ab82e2200400c646e7d471b
Cameron Zach <angrypickle@comcast.net>
2019-04-15 19:34:10 -0400

    use express maybe

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var connection = mysql.createConnection({
     port: 3306
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) {
         console.error('Database connection failed: ' + err.stack);
         return;
@@ -16,12 +16,13 @@ connection.connect(function(err) {
     console.log('Connected to database.');
 });
 
-var http = require('http');
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World\n');
-}).listen(80, "127.0.0.1");
+const express = require('express');
+const app = express();
 
-console.log('Server running at http://127.0.0.1:1337/');
+app.get('/', (req, res) => {
+    res.send('HEY!')
+});
+
+app.listen(3000, () => console.log('Server running on port 3000'));
 
 connection.end();
