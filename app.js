bf4cbac354489922263abfa19df3a18e2eb2e5ea
Cameron Zach <angrypickle@comcast.net>
2019-04-16 11:18:16 -0400

    serve static content

--- app.js
+++ app.js
@@ -19,10 +19,7 @@ connection.connect(function (err) {
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('HEY!')
-});
-
+app.use(express.static('public'));
 app.listen(3000, () => console.log('Server running on port 3000'));
 
 connection.end();
