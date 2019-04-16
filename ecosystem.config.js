df189c0efdee1cd34660192092a9bacd7f9af87f
Cameron Zach <angrypickle@comcast.net>
2019-04-16 01:09:43 -0400

    add pm2 stuff

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    apps: [{
+        name: 'tutorial-2',
+        script: './index.js'
+    }],
+    deploy: {
+        production: {
+            user: 'ec2-user',
+            host: 'http://ec2-54-174-134-52.compute-1.amazonaws.com/',
+            key: '~/.ssh/id_rsa.pub',
+            ref: 'origin/master',
+            repo: 'https://github.com/camzach/get-this-bread.git',
+            path: '~/get-this-bread',
+            'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+        }
+    }
+};
