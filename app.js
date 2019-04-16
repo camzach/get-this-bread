6f26c87159e3218d0f9835acd1acfe024338f882
Cameron Zach <angrypickle@comcast.net>
2019-04-16 11:27:40 -0400

    store tutorial

--- app.js
+++ app.js
@@ -16,10 +16,20 @@ connection.connect(function (err) {
     console.log('Connected to database.');
 });
 
+const store = require('./store');
 const express = require('express');
 const app = express();
 
 app.use(express.static('public'));
 app.listen(3000, () => console.log('Server running on port 3000'));
+
+app.post('/createUser', (req, res) => {
+    store
+        .createUser({
+            username: req.body.username,
+            password: req.body.password
+        })
+        .then(() => res.sendStatus(200))
+});
 
 connection.end();
