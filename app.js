a46abdb5dad0a66cdeb66e77fd547f32e0c97df3
Cameron Zach <angrypickle@comcast.net>
2019-04-16 11:37:16 -0400

    add body-parser

--- app.js
+++ app.js
@@ -18,8 +18,11 @@ connection.connect(function (err) {
 
 const store = require('./store');
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
 
+app.use(bodyParser.json());
 app.use(express.static('public'));
 app.listen(3000, () => console.log('Server running on port 3000'));
 
