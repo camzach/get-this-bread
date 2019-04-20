a7ac225bf9909923fdd9d841ceee0f6c5ed3f3b0
Cameron Zach <angrypickle@comcast.net>
2019-04-20 01:18:49 -0400

    add delete form to site, and rename phoneNumber to phone for consistency

--- public/app.js
+++ public/app.js
@@ -2,8 +2,15 @@ const CreateUser = document.querySelector('.CreateUser');
 CreateUser.addEventListener('submit', (e) => {
     e.preventDefault();
     const nickname = CreateUser.querySelector('.nickname').value;
-    const phoneNumber = CreateUser.querySelector('.phone').value;
-    post('/createUser', {nickname, phoneNumber})
+    const phone = CreateUser.querySelector('.phone').value;
+    post('/createUser', {nickname, phone})
+});
+
+const DeleteUser = document.querySelector('.DeleteUser');
+DeleteUser.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const phone = DeleteUser.querySelector('.phone').value;
+    post('/deleteUser', {phone})
 });
 
 function post(path, data) {
